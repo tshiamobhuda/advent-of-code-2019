4e22ed83c8c162b1ed445535a29724ebce433ca0
Tshiamo <tshiamobhuda@gmail.com>
2019-12-08 20:21:59 +0200

    Day 3: Moved common function to module

--- src/day3/a.py
+++ src/day3/a.py
@@ -1,4 +1,5 @@
 import os
+import common
 
 lines = []
 paths = []
@@ -14,41 +15,8 @@ while True:
 
 f.close()
 
-def buildLinePath(line):
-    path = []
-    x = 0
-    y = 0
-
-    for coordinate in line:    
-        direction = coordinate[0]
-        move = int(coordinate[1::])
-
-        if direction == 'R':
-            for i in range(x, (x+move)):
-                path.append((i, y))
-            x += move
-
-        elif direction == 'L':
-            for i in range(x, (x-move), -1):
-                path.append((i, y))
-            x -= move
-
-        elif direction == 'U':
-            for i in range(y, (y+move)):
-                path.append((x, i))
-            y += move
-
-        elif direction == 'D':
-            for i in range(y, (y-move), -1):
-                path.append((x, i))
-            y -= move
-    else:
-        path.append((x, y))
-
-    return path
-
 for line in lines:
-    paths.append(buildLinePath(line.split(',')))
+    paths.append(common.buildLinePath(line.split(',')))
 
 intersections = set(paths[0]).intersection(paths[1])
 
