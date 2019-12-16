fd476a9fe415fbf6cb7cbf86bd1ce23b387309b6
Tshiamo <tshiamobhuda@gmail.com>
2019-12-16 22:29:29 +0200

    Day 8 Part B - refactored code - paint each row instead of printing each layer

--- src/day8/b.py
+++ src/day8/b.py
@@ -1,50 +1,32 @@
 from a import buildLayers
 
-def buildFinalImage(layers):
-    
-    finalImage = []
-    
-    for layer in range((len(layers)-1), -1, -1):
-        tempLayer = []  
+def buildImage(layers):
+    image = []
 
-        for row in range(len(layers[layer])):
-            tempRow = []
-            for col in range(len(layers[layer][row])):
-                tempRow.insert(col, layers[layer][row][col])
+    for row in range(6):
+        tempRow = []
+        for col in range(25):
+            tempCol = ''
+            for layer in range((len(layers)-1), -1, -1):
+                pixel = layers[layer][row][col]
+                if pixel == 0: #black
+                    tempCol = " "
+                elif pixel == 1: #white
+                    tempCol = "*"
+            tempRow.insert(col, tempCol)
 
-            tempLayer.insert(row, tempRow)
-        
-        if len(finalImage) == 0:
-            finalImage.extend(tempLayer)
-        else:
-            for row in range(len(tempLayer)):
-                for col in range(len(tempLayer[row])):
-                    pixel = tempLayer[row][col]
+        image.insert(row, tempRow)
 
-                    if pixel == 0: #black
-                        finalImage[row][col] = " "
-                    elif pixel == 1: #white
-                        finalImage[row][col] = "*"
-
-    printImage(finalImage)
+    printImage(image)
 
 def printImage(img):
-
-    for row in range(len(img)):
-        for col in range(len(img[row])):
-            pixel = img[row][col]
-            if pixel == 0: #black
-                img[row][col] = " "
-            elif pixel == 1: #white
-                img[row][col] = "*"
-
     for row in range(len(img)):
         for col in range(len(img[row])):
             print(img[row][col], end='')
         print('')
 
 def main():
-    buildFinalImage(buildLayers())
+    buildImage(buildLayers())
 
 if __name__ == "__main__":
     main()
